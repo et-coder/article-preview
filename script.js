7fa9fe67e94ca721aa9ebe23db29ea283128db7a
Nati <thecodegeek1@gmail.com>
2025-09-02 12:41:29 +0300

    fix share button tooltip

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const path = share_btn.querySelector('path');
 const bottom = document.querySelector('.bottom');
 
 share_btn.addEventListener('click', () => {
-    share_btn.style.fill = 'white';
-    share.style.display = 'flex';
-    share_btn.style.backgroundColor = 'var(--Desaturated-Dark-Blue)';
-    path.style.fill = 'white';
-    bottom.style.margin = '0 0 0.6em';
+    if (!share.classList.contains('active')) {
+        share.classList.add('active');
+        share_btn.style.backgroundColor = 'var(--Desaturated-Dark-Blue)';
+        path.style.fill = 'white';
+        // bottom.style.margin = '0 0 0.6em';
+    } else {
+        share.classList.remove('active');
+        path.style.fill = 'var(--Desaturated-Dark-Blue';
+        share_btn.style.backgroundColor = 'var(--Light-Grayish-Blue)';
+    }
 })
 
